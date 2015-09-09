3ea8f344e89dc7b141210cd27abe6f47751273ef
Xin-Xin Wang - blueberry <xin-xinw@musiclifephilosophy.com>
2015-09-09 14:14:55 -0400

    test-suite-demo 1.0

---
+++ tests.js
@@ -0,0 +1,19 @@
+title("Test Suite Testing 1.0");
+
+//// Sanity ////
+
+test('true', false); // should fail
+test('123', 123);
+test('false', false);
+test('true', true);
+
+function iso(a, b){
+  if (a.length != b.length)return false;
+  for (var i = 0; i < a.length; i++){
+    if (a[i] !== b[i])return false;
+  }
+  return true;
+}
+
+test('[1, 2, 3]', [1, 2, 3], iso);
+
